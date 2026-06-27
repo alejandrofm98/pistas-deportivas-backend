a0c689509476cc826ab3454442322d545b7031d4
alejandrofm98 <alejandrofmayor98@gmail.com>
2026-06-27 18:34:02 +0200

    feat: Eliminar método de pago 'ONSITE' (develop)
    Elimina el método de pago 'ONSITE' de la aplicación.
    Actualiza esquema de DB, enum y lógica asociada.

--- src/main/resources/db/migration/V10__remove_onsite_payment_method.sql
+++ src/main/resources/db/migration/V10__remove_onsite_payment_method.sql
@@ -1,4 +1,6 @@
+DELETE FROM reservations WHERE payment_method = 'ONSITE';
+
 ALTER TABLE reservations DROP CONSTRAINT IF EXISTS reservations_payment_method_check;
 
 ALTER TABLE reservations ADD CONSTRAINT reservations_payment_method_check
-    CHECK (payment_method IN ('ONLINE', 'BIZUM', 'ONSITE'));
+    CHECK (payment_method IN ('ONLINE', 'BIZUM'));
